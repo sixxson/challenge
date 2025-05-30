2dee43fe84abea2aa47d9201384947e94e34a21b
minh le <117999133+sixxson@users.noreply.github.com>
2025-05-31 00:21:54 +0700

    update close web delete phone in localStrorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 export default function App() {
-  return (
-    <>
-      <Outlet />
-    </>
-  );
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      localStorage.removeItem("phone");
+      localStorage.removeItem("otpVerified")
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  return <Outlet />;
 }
